397317250ef99e241f882949d078fd1a5f7aab42
BlueFide <70350411+BlueFide@users.noreply.github.com>
2022-10-04 21:28:08 -0400

    Assignment Completed

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -27,7 +27,14 @@ class DrivableMap {
      *       in drivable_map, then add the pair to drivable_map.
      *       Return true if the Drivable was added to drivable_map.
      */
-
+    public boolean addDrivable(String id, Drivable object) {
+        if (drivable_map.get(id) == null) {
+            this.drivable_map.put(id, object);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 
 
@@ -37,7 +44,16 @@ class DrivableMap {
      * You may want to use drivable_map.keys() or drivable_map.values() to
      * iterate through drivable_map.
      */
+    public boolean hasFasterThan(int speed) {
 
+        for (Drivable object: drivable_map.values()) {
+            if (object.getMaxSpeed() >= speed) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 
 
 
@@ -46,7 +62,17 @@ class DrivableMap {
      *       returns a List containing all of the Tradable items in
      *       drivable_map.
      */
+    public List<Tradable> getTradable() {
+        List<Tradable> tradableItems = new ArrayList<Tradable>();
 
+        for (Drivable object: this.drivable_map.values()) {
+            if (object instanceof Tradable) {
+                tradableItems.add((Tradable) object);
+            }
+        }
+
+        return tradableItems;
+    }
 
 
     
